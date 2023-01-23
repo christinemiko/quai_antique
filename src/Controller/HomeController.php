838fc5e029da5a93685c4a6489678933340f809e
Christine Chau <christinemiko@gmail.com>
2023-01-23 19:24:33 +0100

    homepage

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+ namespace App\Controller;
+
+ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+ use Symfony\Component\HttpFoundation\Response;
+ use Symfony\Component\Routing\Annotation\Route;
+
+
+ class HomeController extends AbstractController
+ {
+
+     #[Route('Accueil')]
+     public function Accueil() : Response
+     {
+         return $this->render("homepage.html.twig");
+     }
+ }
