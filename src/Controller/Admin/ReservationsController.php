4f2c46630c40af04f87b48bc92dde91012f761df
Christine Chau <christinemiko@gmail.com>
2023-02-26 18:35:38 +0100

    add reservation admin twig

--- src/Controller/Admin/ReservationsController.php
+++ src/Controller/Admin/ReservationsController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Repository\HourRepository;
+use App\Repository\ReservationRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,12 +13,18 @@ class ReservationsController extends AbstractController
 {
 
     #[Route('/admin/reservations', name: 'app_admin_reservations')]
-    public function showreservations (HourRepository $hourRepository): Response
+    public function showreservation(HourRepository $hourRepository, ReservationRepository $reservationRepository, UserRepository $userRepository): Response
     {
         $hourFixtures = $hourRepository->find(33);
+        $reservations = $reservationRepository->findAll();
+
+        $reservationsTest =  $reservations[0];
+        dump( $reservationsTest->getUser());
+
 
         return $this->render('admin/reservations.html.twig', [
             'hourFixtures' => $hourFixtures,
+            'reservations' => $reservations
         ]);
     }
 }
