32bbcaff8b4d7dc3ef79164f023f82aba5486cd6
Christine Chau <christinemiko@gmail.com>
2023-02-26 21:48:34 +0100

    add menus admin twig

--- src/Controller/Admin/MenusController.php
+++ src/Controller/Admin/MenusController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Admin;
 
 use App\Repository\HourRepository;
+use App\Repository\MenuRepository;
+use App\Repository\ProductMenuRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,13 +14,38 @@ class MenusController extends AbstractController
 {
 
     #[Route('/admin/menus', name: 'app_admin_menus')]
-    public function showhours (HourRepository $hourRepository): Response
+    public function getProductMenus(ProductMenuRepository $repository, MenuRepository $menuRepository, HourRepository $hourRepository): Response
     {
         $hourFixtures = $hourRepository->find(33);
 
+        $menu = $menuRepository->find(62);
+        dump($menu->getProductMenus());
+
+        $productMenus = $repository->findAll();
+
+
+        $productMenuTest = $productMenus[0];
+        dump($productMenuTest->getProduct()->getCategory()->getNameCategory());
+
         return $this->render('admin/menus.html.twig', [
+            'productMenus' => $productMenus,
+            'menu' => $menu,
             'hourFixtures' => $hourFixtures,
         ]);
     }
+
+    #[Route('/admin/createmenus', name: 'app_admin_createmenus')]
+    public function index (HourRepository $hourRepository, MenuRepository $menuRepository): Response
+    {
+        $hourFixtures = $hourRepository->find(33);
+        $menus = $menuRepository->findAll();
+
+        return $this->render('admin/createmenus.html.twig', [
+
+            'hourFixtures' => $hourFixtures,
+            'menus' => $menus,
+        ]);
+    }
+
 }
 
