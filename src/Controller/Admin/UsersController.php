2e901038880cf277927c6ad1edf8b595629a7c03
Christine Chau <christinemiko@gmail.com>
2023-02-26 19:02:31 +0100

    add Clients admin twig

--- src/Controller/Admin/UsersController.php
+++ src/Controller/Admin/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Repository\HourRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,12 +12,15 @@ class UsersController extends AbstractController
 {
 
     #[Route('/admin/users', name: 'app_admin_users')]
-    public function showusers (HourRepository $hourRepository): Response
+    public function showusers (HourRepository $hourRepository, UserRepository $userRepository): Response
     {
         $hourFixtures = $hourRepository->find(33);
+        $users = $userRepository->findAll();
 
         return $this->render('admin/users.html.twig', [
+
             'hourFixtures' => $hourFixtures,
+            'users' => $users,
         ]);
     }
 }
