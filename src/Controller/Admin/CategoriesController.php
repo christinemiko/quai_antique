8a9d400489704aa70def3636dec2236f2c57aea5
Christine Chau <christinemiko@gmail.com>
2023-02-28 21:21:44 +0100

    add new category

--- src/Controller/Admin/CategoriesController.php
+++ src/Controller/Admin/CategoriesController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Category;
+use App\Entity\Hour;
+use App\Form\CategoriesFormType;
+use App\Form\HoursFormType;
 use App\Repository\CategoryRepository;
 use App\Repository\HourRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,7 +19,7 @@ class CategoriesController extends AbstractController
 {
 
     #[Route('/admin/categories', name: 'app_admin_categories')]
-    public function showcategories (HourRepository $hourRepository, CategoryRepository $categoryRepository,PaginatorInterface $paginator, Request $request): Response
+    public function index (HourRepository $hourRepository, CategoryRepository $categoryRepository,PaginatorInterface $paginator, Request $request): Response
     {
         $hourFixtures = $hourRepository->find(33);
 
@@ -29,6 +34,34 @@ class CategoriesController extends AbstractController
             'categorys' => $categorys,
         ]);
     }
+
+    #[Route('/admin/newcategories', name: 'app_admin_newcategories', methods: ['GET', 'POST'])]
+    public function newCategories(HourRepository $hourRepository,Request $request, EntityManagerInterface $entityManager, CategoryRepository $categoryRepository): Response
+    {
+        $hourFixtures = $hourRepository->find(33);
+
+        $categories = new Category ();
+
+        $form = $this->createForm(CategoriesFormType::class, $categories);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $categories = $form->getData();
+            $entityManager->persist($categories);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('categories');
+        }
+
+        return $this->render('admin/newcategories.html.twig', [
+
+            'hourFixtures' => $hourFixtures,
+            'form' => $form->createView()
+
+        ]);
+    }
+
+
 
 }
 
